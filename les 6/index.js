2837c40cb907616222826591485d394815a7c386
Musaddiq <musaddiq3600@gmail.com>
2023-08-15 23:11:08 +0500

    6

---
+++ les 6/index.js
@@ -0,0 +1,44 @@
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
+
+// data
+import db from './_db.js'
+
+// types
+import { typeDefs } from './schema.js'
+
+// resolvers
+const resolvers = {
+  Query: {
+    games() {
+      return db.games
+    },
+    game(_, args) {
+      return db.games.find((game) => game.id === args.id)
+    },
+    authors() {
+      return db.authors
+    },
+    author(_, args) {
+      return db.authors.find((author) => author.id === args.id)
+    },
+    reviews() {
+      return db.reviews
+    },
+    review(_, args) {
+      return db.reviews.find((review) => review.id === args.id)
+    }
+  }
+}
+
+// server setup
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 }
+})
+
+console.log(`Server ready at: ${url}`)
---
+++ les 6/schema.js
@@ -0,0 +1,25 @@
+export const typeDefs = `#graphql
+  type Game {
+    id: ID!
+    title: String!
+    platform: [String!]!
+  }
+  type Review {
+    id: ID!
+    rating: Int!
+    content: String!
+  }
+  type Author {
+    id: ID!
+    name: String!
+    verified: Boolean!
+  }
+  type Query {
+    games: [Game]
+    game(id: ID!): Game
+    reviews: [Review]
+    review(id: ID!): Review
+    authors: [Author]
+    author(id: ID!): Author
+  }
+`
